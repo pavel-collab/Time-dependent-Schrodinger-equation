dede1e60353c764bc701a38163cb150f7a4098ff
Pavel Filippenko <rubikpf2002@mail.ru>
2022-12-31 19:23:21 +0300

    Написал комментарии к функциям и методам в подключаемых модулях.

--- models/include/PotentialBarriers.py
+++ models/include/PotentialBarriers.py
@@ -1,46 +1,81 @@
 import numpy as np
 
-# вероятность волнового пакета преодолеть барьер (прямоугольный)
-# при заданном значении высоты и ширины барьера
-# а так же при заданном значении энергии частицы
-#! Рассмотреть ситуацию, когда V0 < E (вероятно этот случай надо учесть отдельно в теоретическом смысле)
-def transmission_probability(E, V0, a):
-    """Transmission probability of through a rectangular potential barrier."""
-    k = (V0 * np.sinh(a * np.sqrt(2 * (V0 - E))))**2 / (4 * E * (V0 - E))
-    return 1 / (1 + k)
+'''
+File contains a functions of different potential barriers. 
+File should be imported to target model.
+Every function return value consist of coordinate, this mean that functions work with
+values, not with numpy arrays of lists.
 
-'''examples of potential pit'''
-# the method is a x function
-# use 
-# V_dense = np.array([V(x) for x in x_dense])
+Example how to use fucntion V(x) (x_dense -- numpy array of space range):
+V_dense = np.array([V(x) for x in x_dense])
+'''
 
-# box pit
-# x -- x range, x0 -- center of pit, d -- width of pit, V0 -- depth
+#------------------------------------------------------------------------------------------
+
+'''
+Box pit.
+x -- space range
+x0 -- center of pit
+d -- width of pit
+V0 -- depth of pit. Note that in this function value V0 must be negative.
+'''
 def BoxBarier(x, x0, d, V0):
     if (x <= x0 - d/2 or x >= x0 + d/2):
         return 0
     else:
         return V0
 
-# Oscillator
+'''
+Parabola potential for harmonic oscillator model.
+x -- space range
+omega -- parabola factor
+'''
 def ParabolaPotential(x, omega):
     return 0.5 * omega**2 * x**2
 
+'''
+Box potential barrier for quantum tunneling.
+x -- space range
+x0 -- center of potential barrir
+d -- width of potential barrier
+V0 -- the hight of potential barrier. Note that in this fuction V0 must be positive.
+'''
 def BoxPotential(x, x0, d, V0):
     if (x <= x0 - d/2 or x >= x0 + d/2):
         return V0
     else:
         return 0
 
+'''
+Rump potential with start in 0.
+x -- space range
+k -- ramp potential factor
+'''
 def RampPotential(x, k):
     return k*x
 
+'''
+Step potential.
+x -- space range
+x0 -- coordinate of potential step
+V1 -- the first value of potential
+V2 -- the first value of potential
+'''
 def StepPotential(x, x0, V1, V2):
     if x <= x0:
         return V1
     else: 
         return V2
 
+'''
+Box potential barrier for quantum tunneling with different potential levels.
+x -- space range
+x0 -- center of potential barrir
+d -- width of potential barrier
+V0 -- the first potential level (before potential barrier)
+V1 -- the second potential level (potential barrier)
+V2 -- the third potential level (after potential barrier)
+'''
 def TwoLevelBoxPotential(x, x0, d, V0, V1, V2):
     if x <= x0 - d/2:
         return V0
@@ -50,8 +85,14 @@ def TwoLevelBoxPotential(x, x0, d, V0, V1, V2):
         return V2
 
 '''
-x1 and x2 -- centers of potential walls
-x1 must be less then x2
+Two box potential parriers one by one.
+x -- space range
+x1 -- center of the first potential barrir
+x2 -- center of the second potential barrir
+d1 -- width of the first potential barrier
+d2 -- width of the second potential barrier
+V1 -- the hight of the first potential barrier. Note that in this fuction V0 must be positive.
+V2 -- the hight of the second potential barrier.
 '''
 def TwoWall(x, x1, x2, d1, d2, V1, V2):
     if (x >= x1 - d1/2) and (x <= x1 + d1/2):
@@ -60,3 +101,15 @@ def TwoWall(x, x1, x2, d1, d2, V1, V2):
         return V2
     else: 
         return 0
+
+'''
+The propability of wave package to transmit the potential barrier.
+The propability consists of barriers hight and width, and also of portisles energy.
+E -- wave package energy
+V0 -- the hight of potential barrier
+a -- width of potential barrier
+'''
+def TransmissionProbability(E, V0, a):
+    """Transmission probability of through a rectangular potential barrier."""
+    k = (V0 * np.sinh(a * np.sqrt(2 * (V0 - E))))**2 / (4 * E * (V0 - E))
+    return 1 / (1 + k)
--- models/include/ShrodingerEquation.py
+++ models/include/ShrodingerEquation.py
@@ -5,52 +5,47 @@ from scipy.sparse import diags, spdiags
 from scipy.linalg import expm
 from scipy import integrate
 
+'''
+Class makes a type of form of psi function like a Gauss wave package.
+'''
 class GaussWavePackage:
-    __slots__ = [
-        '__x', 
-        '__x0',
-        '__sigma0',
-        '__p0'
-    ]
+    __slots__ = ['__x', '__x0', '__sigma0', '__p0']
+
     def __init__(self, x_dense, x0, sigma0, p0):
         self.__x = x_dense
         self.__x0 = x0
         self.__sigma0 = sigma0
         self.__p0 = p0
 
-    #! учитываем, что h = 1
+    # Note, that h = 1 (where h is a Planc constant)
     def GetWavePackage(self):
         A = (2*np.pi*self.__sigma0**2)**(-0.25)
         return A*np.exp(1j*self.__p0*self.__x - ((self.__x-self.__x0)/(2*self.__sigma0))**2)   
 
-    @property # <- применяется для гетеров   
+    @property 
     def sigma(self):
         return self.__sigma0
 
+'''
+Class define the psi fucntion and methods that defines how psi fucntion
+changed during the time.
+'''
 class WaveFunction:
-    __slots__ = [
-        '__x', 
-        '__V', 
-        '__N', 
-        '__dx', 
-        'psi'
-    ]
+    __slots__ = ['__x', '__V', '__N', '__dx', 'psi']
 
-    # инициализируем уравнение начальным значением функции
-    # пространственым интервалом
-    # и видом потенциального барьера
+    # Define the object by initial psi function, space range and potential barrier 
+    # Note, that all three initial values are arrays
     def __init__(self, psi0, x_dense, V):
-        self.psi = psi0.GetWavePackage() #! массив
+        self.psi = psi0.GetWavePackage()
         self.__x = x_dense
         self.__V = V
 
         self.__N = np.shape(x_dense)[0]
-        # предполагаем, что сетка равномерная
+        # Note, that we use a uniform grid
         self.__dx = x_dense[1] - x_dense[0]
 
-    # return a matrix (like a mtrix operator)
-    # здесь предполагаем нормировку постоянной планка на 1
-    # массу частицы m = 1
+    # Privet method return a matrix (like a matrix operator)
+    # Note, that m = 1 and h = 1 
     def __Hamiltonian(self):
         N = self.__N
         dx = self.__dx
@@ -65,39 +60,42 @@ class WaveFunction:
         H = self.__Hamiltonian()
 
         U = expm(-1j*H*dt)
-        # задаем точность -- отрубаем слишком малые элементы матрицы
+        # make a occuracy (set a tiny values to a 0)
         U[(U.real**2 + U.imag**2) < 1e-10] = 0
         return U
 
     def WaveFunctioProbability(self):
         return self.psi.real**2 + self.psi.imag**2  
 
+    # Method define how psi function changed during the time
     def PsiTimeEvolute(self):
         U = self.__TimeEvolution()
         self.psi = U @ self.psi
 
-    @property # <- применяется для гетеров   
+    @property 
     def Hamiltonian(self):
         H = self.__Hamiltonian()
         return H
 
+    # Return an average coordinate of wave package
     def GetAvrgCordinate(self):
-        # значение для интегрирования
+        # value for integration
         intgr_val = self.psi.conjugate() * self.__x * self.psi
         intgr_val = intgr_val.real
 
         average_x = integrate.simps(intgr_val, self.__x)
         return average_x
 
+    # Return an average momentum of wave package
     def GetAvrgMomentum(self):
-        # здесь учитывается, что h = 1
+        # Note, that h = 1
         momentum_operator2psi_funtion = -1j * (np.diff(self.psi, 1) / self.__dx)
-        # значение для интегрирования
-        # поскольку используемая функци np.diff возвращает массив
-        # меньший на 1 длинны от исходного, то и умножать его следует
-        # на массив соответствующей длинны
-        # на конечный результат вычислений это особо не повлияет, поскольку нам все равно
-        # в конечном итоге нужно число
+        '''
+        As we use a function np.diff the result array less for 1 than
+        the initial, than means we have to multiply it for an array with lenght
+        that less for 1 than the lenght of initial array.
+        This trick don't influence for a result, cause we use this arrays to get a number
+        '''
         intgr_val = self.psi.conjugate()[:-1] * momentum_operator2psi_funtion
         intgr_val = np.sqrt(intgr_val.real**2 + intgr_val.imag**2)
 
