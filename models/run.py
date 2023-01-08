c181767edd9084fa68e514067e9c6d6bfd9246ff
Pavel Filippenko <rubikpf2002@mail.ru>
2023-01-08 18:06:18 +0300

    сделал stepping модели RampPotential по коэффициенту k

--- models/run.py
+++ models/run.py
@@ -16,7 +16,10 @@ log_file.write(date + '\n')
 log_file.write('-'*50 + '\n')
 log_file.close()
 
-subprocess.run(["python3", "RampPotential.py", "-i"])
+k_range = np.array([100, 120, 130, 140, 150, 160, 170, 180, 190, 200])
+
+for k in k_range:
+    subprocess.run(["python3", "RampPotential.py", "-k", str(k)])
 
 total_exec_t = datetime.now() - start
 
