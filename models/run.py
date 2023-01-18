0694e242714c6ee0ea0e4e59423bcab997672fc6
Pavel Filippenko <rubikpf2002@mail.ru>
2023-01-18 15:27:28 +0300

    Несколько дней до государственного экзамена. Подготовка к большому эксперементу. Готовимся запускать модели для конечного показа. Я собираюсь поменять некоторые параметры конфигов и увеличить количество фрэймов. Каждое видео должно длиться 20 секунд.

--- models/run.py
+++ models/run.py
@@ -16,7 +16,7 @@ log_file.write(date + '\n')
 log_file.write('-'*50 + '\n')
 log_file.close()
 
-E_range = np.array([0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.49])
+E_range = np.array([0.51, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95, 1.0])
 
 for E in E_range:
     subprocess.run(["python3", "QuantumTunnelling.py", "-i", "-e", str(E)])
