5542e7e055b4ff20e6be93200f5603c59d473d03
Pavel Filippenko <rubikpf2002@mail.ru>
2023-02-01 23:26:26 +0300

    Написал плюсовую реализацию питоновской функции PsiTimeEvolute(). Много ужасных решений. Буду все исправлять, если заработает.

--- lib.cpp
+++ lib.cpp
@@ -1,4 +1,5 @@
 #include <complex>
+#include <algorithm>
 #include "lib.hpp"
 
 /*
@@ -41,9 +42,11 @@ void spdiags(double* arr, int N, double* vals)
     }
 }
 
-double* PsiTimeEvolution(long N, double dx, double* psi, double* V)
+//TODO посмотреть, нет ли готовых функций для сложения массивов. 
+// может быть можно использовать готовые плюсовые контейнеры или библиотеки
+void PsiTimeEvolutionReal(long N, double dx, double* real_psi, double* imag_psi, double* V)
 {
-    double* H = new double[N*N];
+    std::complex<double>* H = new std::complex<double>[N*N];
 
     double* L = new double[N*N];
     double* U = new double[N*N];
@@ -56,12 +59,61 @@ double* PsiTimeEvolution(long N, double dx, double* psi, double* V)
         for (long j = 0; j < N; ++j)
         {
             long idx = i*N + j;
-            H[idx] = (-1/(2*dx*dx)) * L[idx] + U[idx];
+            std::complex<double> z(0, -((-1/(2*dx*dx)) * L[idx] + U[idx]));
+            H[idx] = z;
         }
     }
 
     delete L;
     delete U;
 
-    // на этой стадии у нас есть матрица гамильтониана в виде одномерного массива
+    // на этой стадии у нас есть комплексная матрица гамильтониана в виде одномерного массива
+
+    SqrMatrix<std::complex<double>> Hamiltonian(N, H);
+    Matrix<std::complex<double>> newU = Hamiltonian.exp();
+
+    delete H;
+
+    //TODO привести малые значения к 0
+
+    std::vector<std::complex<double>> vals;
+    // std::vector<double> real_vals;
+    // std::vector<double> imag_vals;
+    vals = newU.data();
+
+    for (int i = 0; i < N*N; ++i)
+    {
+        std::complex<double> z(0, 0);
+        if (vals[i].real()*vals[i].real() + vals[i].imag()*vals[i].imag() < 0.000001)
+            vals[i] = z;
+
+        // real_vals[i] = vals[i].real();
+    }
+
+    std::complex<double>* dst = new std::complex<double>[N];
+
+    for (int i = 0; i < N ; ++i)
+    {
+        std::complex<double> z(real_psi[i], imag_psi[i]);
+        dst[i] = z;
+    }
+
+    std::complex<double>* res = new std::complex<double>[N];
+
+    for (int i = 0; i < N; ++i)
+    {
+        std::complex<double> sum(0, 0);
+        for (int j = 0; j < N; ++j)
+        {
+            sum += vals[i*N+j] * dst[j];
+        }
+
+        res[i] = sum;
+    }
+
+    for (int i = 0; i < N; ++i)
+    {
+        real_psi[i] = res[i].real();
+        imag_psi[i] = res[i].imag();
+    }
 }
