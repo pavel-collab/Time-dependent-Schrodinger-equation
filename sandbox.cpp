94e640827a13597f90b9ab03f87619564c6a6827
Pavel Filippenko <rubikpf2002@mail.ru>
2023-02-04 21:22:39 +0300

    С динамическим выделением памяти вроде все работает нормально. Просто я указывал неверный размер матрицы

--- sandbox.cpp
+++ sandbox.cpp
@@ -6,51 +6,38 @@
 int main()
 {    
     int N = 4;
-    // double* val1 = new double[N];
+    double* val1 = new double[N];
 
-    // std::cout << "checkoint" << std::endl;
+    for (int i = 0; i < N; ++i)
+    {
+        val1[i] = i;
+    }
 
-    // for (int i = 0; i < N; ++i)
-    // {
-    //     val1[i] = i;
-    // }
-
-    // std::cout << "checkoint" << std::endl;
-
-    double val1[4] = {1, 2, 3, 4};
-    SqrMatrix<double> Mat1(N, val1);
+    SqrMatrix<double> Mat1(N/2, val1);
 
     for (int i = 0; i < N; ++i)
     {
         std::cout << Mat1.data()[i] << std::endl;
     }
 
-    std::cout << "checkoint" << std::endl;
 
     Matrix<double> res1 = Mat1.exp();
 
-    std::cout << Mat1.exp() << std::endl;
-    // for (int i = 0; i < N; ++i)
-    // {
-    //     std::cout << res1.data()[i] << std::endl;
-    // }
+    std::cout << res1 << std::endl;
 
-    // std::complex<double>* val2 = new std::complex<double>[N];
+    std::complex<double>* val2 = new std::complex<double>[N];
 
-    // for (int i = 0; i < N; ++i)
-    // {
-    //     std::complex<double> z(i, i+1);
-    //     val2[i] = z;
-    // }
+    for (int i = 0; i < N; ++i)
+    {
+        std::complex<double> z(i, i+1);
+        val2[i] = z;
+    }
 
-    // SqrMatrix<std::complex<double>> Mat2(N, val2);
-    // Mat2.exp();
-    // for (int i = 0; i < 10; ++i)
-    // {
-    //     std::cout << Mat2.data()[i].real() << std::endl;
-    // }
+    SqrMatrix<std::complex<double>> Mat2(N/2, val2);
+    Matrix<std::complex<double>> res2 = Mat2.exp();
+    std::cout << res2 << std::endl;
 
-    // delete val1;
-    // delete val2;
+    delete val1;
+    delete val2;
     return 0;
 }
