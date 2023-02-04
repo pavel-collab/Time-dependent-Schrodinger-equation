e1f6627288fe4ca97e3a9cb9c1249c8585cb0aab
Pavel Filippenko <rubikpf2002@mail.ru>
2023-02-04 21:27:42 +0300

    При большом N > 250 выскакивает ошибка сигментирования. Пока не могу понять, почему это так. Не может же быть такого, что я прям всю память загружаю. Или может...

--- sandbox.cpp
+++ sandbox.cpp
@@ -5,7 +5,7 @@
 
 int main()
 {    
-    int N = 4;
+    int N = 250;
     double* val1 = new double[N];
 
     for (int i = 0; i < N; ++i)
@@ -20,24 +20,27 @@ int main()
         std::cout << Mat1.data()[i] << std::endl;
     }
 
+    // normilize
+    Mat1 /= N;
+
 
     Matrix<double> res1 = Mat1.exp();
 
     std::cout << res1 << std::endl;
 
-    std::complex<double>* val2 = new std::complex<double>[N];
+    // std::complex<double>* val2 = new std::complex<double>[N];
 
-    for (int i = 0; i < N; ++i)
-    {
-        std::complex<double> z(i, i+1);
-        val2[i] = z;
-    }
+    // for (int i = 0; i < N; ++i)
+    // {
+    //     std::complex<double> z(i, i+1);
+    //     val2[i] = z;
+    // }
 
-    SqrMatrix<std::complex<double>> Mat2(N/2, val2);
-    Matrix<std::complex<double>> res2 = Mat2.exp();
-    std::cout << res2 << std::endl;
+    // SqrMatrix<std::complex<double>> Mat2(N/2, val2);
+    // Matrix<std::complex<double>> res2 = Mat2.exp();
+    // std::cout << res2 << std::endl;
 
     delete val1;
-    delete val2;
+    // delete val2;
     return 0;
 }
